4da1de81e12a89a6862c92a1e911c228566b646c
Tuğçe Tuğçe <etugceorge@gmail.com>
2025-05-03 04:52:26 +0300

    created model

--- MeetingApp/Controllers/HomeController.cs
+++ MeetingApp/Controllers/HomeController.cs
@@ -1,9 +1,37 @@
+using MeetingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace MeetingApp.Controllers
 {
-    public class HomeController: Controller {
-        public string Index(){
-            return "Home/Index";
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+            /* viewbag verilerini return a 
+            int saat = DataTime.Now.Hour;
+            ViewBag.Selamlama = saat > 12 ? "İyi Günler" : "Günaydın"; bu şekilde view de direkt @ViewBag.Selamlama yazıp kullan
+            ViewBag.UserName = "Elif Tuğçe";
+            return View();
+            
+            */
+            /*     var selamlama = saat > 12 ? "İyi Günler":"Günaydın"; 
+            return View(model : selamlama);
+            bu şekilde stringi model olarak gönder
+            ama view de başa bunu yaz => @model string */
+            /*
+            ViewData key value şeklinde olan
+            view da @ViewData["Selamlama"] şklinde kullan
+            */
+            ViewData["Selamlama"] = saat > 12 ? "İyi Günler":"Günaydın";
+            ViewData["UserName"] = "Elif";
+            var meetingInfo = new MeetingInfo()
+            {
+                Id = 1,
+                Location = " İstanbul , Abc Kongre Merkezi",
+                Date = new DateTime(2024, 12, 20, 20, 0, 0),
+                NumberOfPeople = 100
+            };
+            return View(meetingInfo);
         }
     }
 }
--- MeetingApp/Program.cs
+++ MeetingApp/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllerWithViews();//mvc şablonu eklemek
+builder.Services.AddControllersWithViews();//mvc şablonu eklemek
 var app = builder.Build();
+app.UseStaticFiles();//wwwwroot altındaki static dosyaları kullanmak için gerekli
+app.UseRouting();
 
 //app.MapGet("/", () => "Hello World!");
 //app.MapDefaultControllerRoute();cont/action/id kısayolu
